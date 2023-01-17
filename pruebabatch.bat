b399c63648f3821adf1807310e82748593e00ca2
Alex <machupichu0409@gmail.com>
2023-01-17 19:39:43 +0100

    first commit

---
+++ pruebabatch.bat
@@ -0,0 +1,13 @@
+@echo off
+tasklist>servicios.txt
+set servicios=0
+set consola=0
+
+
+for /f "skip=3 tokens=3" %%a in (servicios.txt) do (
+if "%%a" equ "Services" set /a servicios+=1
+if "%%a" equ "Console" set /a consola+=1
+)
+echo Tareas de servicio hay %servicios%
+echo Tares por consola hay %servicios%
+pause>nul
