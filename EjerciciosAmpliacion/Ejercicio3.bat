15815efb8e96841a0ec01fcc8fdb68aee32bb677
Alex <machupichu0409@gmail.com>
2023-01-18 21:00:02 +0100

    v4

--- EjerciciosAmpliacion/Ejercicio3.bat
+++ EjerciciosAmpliacion/Ejercicio3.bat
@@ -6,15 +6,16 @@ echo pares (1)
 echo nones (2)
 set /p eleccion1=""
 set /p eleccion2="Saca un numero: "
+cls
 echo maquina=%maquina%
 echo tu=%eleccion2%
 set /a suma=maquina+eleccion2
 echo suma igual a %suma%
 set /a resto="suma %% 2"
 if %resto% EQU 0 (
-echo ganan pares
+echo Ganan pares
 ) else (
-echo ganan nones
+echo Ganan nones
 )
 if %eleccion1% equ 1 (
 echo HAS GANADO
